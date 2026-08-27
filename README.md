25d348033afc9933d1a83c265b9a31251c05234d
Tale Prestmo <tale.prestmo@gmail.com>
2026-08-27 10:00:19 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# fagsystem-slackbot
+
+Enkel slack app som sender ut meldinger til Team Fagsystem sine kanaler.
---
+++ src/workingplace/workplace-poll-job.js
@@ -0,0 +1,52 @@
+const CronJob = require("cron").CronJob;
+const initWorkplaceBlocks = require("./workplace-blocks");
+
+const TIMEZONE = "Europe/Oslo";
+const SLACK_CHANNELS = ["team-efterlatte-privat"];
+
+const now = () => {
+  return new Date().toLocaleTimeString("no-NO", { timeZone: TIMEZONE });
+};
+
+const setupJob = (app) => {
+  const onTick = async () => {
+    console.log(`Running job @ ${now()}`);
+
+    let title;
+    if (new Date().getDay() === 5) {
+      title = "Endelig helg! :star-struck: Hvor skal du jobbe på mandag?";
+    } else {
+      title = "Hvor skal du jobbe i morgen?";
+    }
+
+    for (const channel of SLACK_CHANNELS) {
+      try {
+        const result = await app.client.chat.postMessage({
+          channel: channel,
+          blocks: initWorkplaceBlocks(title),
+          text: "Should display blocks containing buttons to select workplace",
+        });
+        if (result.ok) {
+          console.log(`Message sent OK to #${channel}`);
+        } else {
+          console.error(
+            `Error on postMessage to channel ${channel}: ${result.error}`,
+          );
+        }
+      } catch (e) {
+        console.error(e);
+      }
+    }
+  };
+
+  // const time = '0 */5 10 * * 1-5' // Test cron
+  const time = "00 55 13 * * 1-5"; // kl 13:55:00, man-fre, alle uker, alle måneder
+
+  console.log(`Init cronjob with crontime: ${time}`);
+
+  const job = new CronJob(time, onTick, null, false, TIMEZONE);
+
+  job.start();
+};
+
+module.exports = setupJob;
