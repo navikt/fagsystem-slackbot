ec2d675eae10c294dfa02d0fb2a07f3aadb66c49
Tale Prestmo <tale.prestmo@gmail.com>
2026-09-01 21:36:10 +0200

    test

--- src/workplace/workplace-poll-job.js
+++ src/workplace/workplace-poll-job.js
@@ -37,7 +37,7 @@ const setupJob = (app) => {
     }
   };
 
-  const time = "00 19 14 * * 1-5"; // kl 13:55:00, man-fre, alle uker, alle måneder
+  const time = "00 37 21 * * 1-5"; // kl 13:55:00, man-fre, alle uker, alle måneder
 
   console.log(`Init cronjob with crontime: ${time}`);
 
